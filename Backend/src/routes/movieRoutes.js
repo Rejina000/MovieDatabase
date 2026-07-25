32ae92254f647bd694db3e28bfc2ca1f26721d12
Rejina <your-verified-email@example.com>
2026-07-25 18:59:03 +0545

    Protect movie routes using JWT middleware

--- Backend/src/routes/movieRoutes.js
+++ Backend/src/routes/movieRoutes.js
@@ -7,14 +7,15 @@ import {
 } from "../controllers/movieController.js";
 
 import { movieRules, validate } from "../validators/movieValidator.js";
+import authenticate from "../utils/middleware/auth.js"; // <-- Add this
 
 const router = express.Router();
 
 router.get("/", getMovies);
 router.get("/:id", getMovieById);
 
-router.post("/", movieRules, validate, createMovie);
+router.post("/", authenticate, movieRules, validate, createMovie);
 
-router.put("/:id", movieRules, validate, updateMovie);
+router.put("/:id", authenticate, movieRules, validate, updateMovie);
 
 export default router;
